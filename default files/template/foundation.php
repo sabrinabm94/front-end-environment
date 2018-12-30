9dd3917f81c083e13ffab54315f648f25c7e3c4d
sabrinabgbc <sabrinabgbc@email.com>
2018-12-30 08:14:31 -0200

    ajustes de resets, style e estrutura dos task runners

---
+++ default files/template/foundation.php
@@ -0,0 +1,6 @@
+<?php include 'header.php'; ?>
+
+div.pg-name>(section.box.name>div.content>(div.row>(div.small-12.medium-6.large-6.columns>div.img>img)+(div.small-12.medium-6.large-6.columns>div.txt>(h1.title)+(h2.subtitle)+button.button.bt-dark)))+(section.box.news>div.content>((h1.title)+(article.article>(header.article-header>(h2.title+date.date+p.author))+(p+p)+(footer.article-footer>((h2.title)+(p))))))
+
+
+<?php include 'footer.php'; ?>
---
+++ default files/template/header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml"> 
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title></title>
+	<meta name=description content=""/>
+	<meta name=keywords content=""/>
+	<meta name="viewport" content="width=device-width,initial-scale=1.0">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/foundation/6.4.3/css/foundation.min.css">
+	<link rel="stylesheet" href="assets/css/main.css"/>
+	<link rel="stylesheet" href="assets/dist/style.css"/>
+</head>
+<body>
+	<header class="header">
+	</header>
